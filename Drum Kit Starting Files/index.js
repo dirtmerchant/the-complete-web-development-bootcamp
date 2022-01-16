165927b8e8827fb15e1f85e67111221df9da3d8f
Bert Bullough <bert@bertbullough.com>
2022-01-16 08:58:21 -0700

    adds a for loop to handle clicks on drum buttons

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,5 +1,10 @@
 alert("Hello world.");
 
-function handleClick() {
-    alert("I got clicked.");
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    alert("I got clicked");
+  });
 }
