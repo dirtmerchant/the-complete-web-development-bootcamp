ebd3e3d8067a8a276fb3845b20bf0e7a686c8df4
Bert Bullough <bert@bertbullough.com>
2022-01-15 19:44:54 -0700

    adds emojis

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,21 +1,21 @@
-// let randomResult = getRandomNumber();
-
-// function getRandomNumber() {
-//   let randomNumber1 = Math.random();
-//   randomNumber1 = randomNumber1 * 6;
-//   randomNumber1 = Math.floor(randomNumber1) + 1;
-  
-//   return randomNumber1;
-// }
-// console.log(randomResult)
-
 let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector(".dice .img1").setAttribute("src", `images/\dice${randomNumber1}.png`);
+document
+  .querySelector(".dice .img1")
+  .setAttribute("src", `images/\dice${randomNumber1}.png`);
 
 let randomNumber2 = Math.floor(Math.random() * 6) + 1;
-document.querySelector(".dice .img2").setAttribute("src", `images/\dice${randomNumber2}.png`);
 
-//let randomNumber1 = Math.floor(Math.random() * 6) + 1;
-//document.querySelector(".dice .img2").setAttribute("src", `images/\dice${randomNumber1}.png`);
+document
+  .querySelector(".dice .img2")
+  .setAttribute("src", `images/\dice${randomNumber2}.png`);
 
+if (randomNumber1 > randomNumber2) {
+    document.querySelector(".heading").innerHTML = "🚩Player 1 Wins";
+}
+else if (randomNumber1 < randomNumber2) {
+    document.querySelector(".heading").innerHTML = "Player 2 Wins🚩";
+}
+else {
+    document.querySelector(".heading").innerHTML = "🚩Draw🚩";
+}
