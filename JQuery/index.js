b3f5dc073726a0111db250a8d43ff9e862918cbb
Bert Bullough <bert@bertbullough.com>
2022-02-07 19:16:51 -0700

    jquery tutorial

--- JQuery/index.js
+++ JQuery/index.js
@@ -1,3 +1,39 @@
-$(document).ready(function () {
-  $("h1").css("color", "red");
+// $(document).ready(function () {
+//   $("h1").css("color", "red");
+// });
+
+// $("a").attr("href", "http://www.bing.com");
+
+// $("h1").click(function () {
+//   $("h1").css("color", "purple");
+// });
+// for (var i = 0; i < 5; i++) {
+//   document.querySelectorAll("button")[i].addEventListener("click", function () {
+//     document.querySelector("h1").style.color = "purple";
+//   });
+// }
+
+// $("button").click(function () {
+//   $("h1").css("color", "purple");
+// });
+
+// $(document).keypress(function (event) {
+//   $("h1").text(event.key);
+// });
+
+// $("h1").on("mousover", function () {
+//   $("h1").css("color", "purple");
+// });
+
+// $("h1").on("mouseover", function () {
+//   $("h1").css("color", "purple");
+// });
+// $("h1").before("<button>New</button>");
+// $("h1").after("<button>New</button>");
+// $("h1").prepend("<button>New</button>");
+// $("h1").remove("<button>New</button>");
+// $("button").hide
+
+$("button").click(function () {
+  $("h1").slideUp().slideDown().animate({opacity: 0.5});
 });
