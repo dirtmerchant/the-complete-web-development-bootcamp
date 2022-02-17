e30482554b30636de5571a91fdd85f548eb56e80
dirtmerchant <bert@bertbullough.com>
2022-02-17 15:57:24 -0700

    game over

--- simon/game.js
+++ simon/game.js
@@ -1,24 +1,15 @@
-var buttonColours = ["red", "blue", "green", "yellow"];
+var buttonColors = ["red", "blue", "green", "yellow"];
+
 var gamePattern = [];
 var userClickedPattern = [];
+
 var started = false;
-
-// 2. Create a new variable called level and start at level 0.
-
 var level = 0;
-
-// 1. Use jQuery to detect when a keyboard key has been pressed, when that happens for the first time, call nextSequence().
 
 $(document).keypress(function () {
   if (!started) {
-    // 3. The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
-
     $("#level-title").text("Level " + level);
-
     nextSequence();
-
-    // You'll need a way to keep track of whether if the game has started or not, so you only call nextSequence() on the first keypress.
-
     started = true;
   }
 });
@@ -27,33 +18,45 @@ $(".btn").click(function () {
   var userChosenColor = $(this).attr("id");
   userClickedPattern.push(userChosenColor);
 
-  // console.log(userClickedPattern);
-
   playSound(userChosenColor);
   animatePress(userChosenColor);
+
+  checkAnswer(userClickedPattern.length - 1);
 });
 
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+
+    //4. If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
+    if (userClickedPattern.length === gamePattern.length) {
+      //5. Call nextSequence() after a 1000 millisecond delay.
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+  }
+}
+
 function nextSequence() {
-  // 4. Inside nextSequence(), increase the level by 1 every time nextSequence() is called.
+  //6. Once nextSequence() is triggered, reset the userClickedPattern to an empty array ready for the next level.
+  userClickedPattern = [];
 
   level++;
-
-  // 5. Inside nextSequence(), update the h1 with this change in the value of level.
-
   $("#level-title").text("Level " + level);
 
   var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColor = buttonColours[randomNumber];
+  var randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
 
   $("#" + randomChosenColor)
     .fadeIn(100)
     .fadeOut(100)
     .fadeIn(100);
-
   playSound(randomChosenColor);
-
-  console.log(level);
 }
 
 function playSound(name) {
