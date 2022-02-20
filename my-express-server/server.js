61fe01403edbdf3de93f92c0a3ed84cb4609d380
dirtmerchant <bert@bertbullough.com>
2022-02-20 09:05:26 -0700

    adds multiple routes to express server

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.get("/contact", function (req, res) {
+  res.send("Contact me at: bert@bertbullough.com");
+});
+
+app.get("/about", function (req, res) {
+  res.send("<h1>Nothing to see here</h1>");
+});
+
+app.get("/hobbies", function (req, res) {
+  res.send("<ul><li>Coffee</li><li>code</li><li>beer</li></ul>");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
